33201a838f420348512ad8f24518f18d41a38936
bikash1376 <bikash13763@gmail.com>
2025-12-25 03:24:37 +0530

    added scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { Script } from "next/script";
 
 const jetbrainsMono = JetBrains_Mono({
   variable: "--font-jetbrains-mono",
@@ -22,6 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+    <head>
+     <Script src="https://cloud.umami.is/script.js" data-website-id="4ab95a7b-c6aa-48d5-a646-30591cbc56da" defer />
+    </head>
       <body
         className={`${jetbrainsMono.variable} antialiased bg-neutral-950 text-neutral-200 font-mono`}
       >
